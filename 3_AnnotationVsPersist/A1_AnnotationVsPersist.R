8a764962877b44e16b948cdefb5b6b29e58f81e4
Aleksandr B. Sahakyan <aleksahak@gmail.com>
2019-03-13 14:30:07 +0000

    Changes in subprojects 3_ and 5_.

--- 3_AnnotationVsPersist/A1_AnnotationVsPersist.R
+++ 3_AnnotationVsPersist/A1_AnnotationVsPersist.R
@@ -1,125 +1,160 @@
+# AnnotationVsPersist
 ################################################################################
-#AnnotationVsPersist
+# FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS
+### DIRECTORY STRUCTURE ########################################################
+whorunsit = "LiezelCluster" # "LiezelMac", "LiezelCluster", "LiezelLinuxDesk",
+                            # "AlexMac", "AlexCluster"
 
-################################################################################ 
-#functions to source
-#lib = "/Users/ltamon/ProjectBoard/lib"
+#*** See whether the content of the <lib>, specified below, can be moved to the
+#*** actual project folder for self consistency. Then, specify the location of
+#*** <lib> within the whorunsit block below. If you put the dependent function
+#*** in <lib> into the <objective.dir>/lib location, then the following line
+#*** outside the whorunsit block, after output.dir specification is enough:
+#***    lib = paste0(objective.dir, "/lib")
+
+# Functions to source
+# lib = "/Users/ltamon/ProjectBoard/lib"
 lib = "/t1-home/icbml/ltamon/ProjectBoard/lib"
-#main directory for the task
-#objective.dir = "/Users/ltamon/ProjectBoard/_GenomicContactDynamics/3_AnnotationVsPersist"
-objective.dir = "/t1-data/user/ltamon/ProjectBoard/_GenomicContactDynamics/3_AnnotationVsPersist"
-#annotation file
-#annoFile.dir = "/Users/ltamon/Database/ucsc_tables"
-annoFile.dir = "/t1-home/icbml/ltamon/Database/ucsc_tables"
-#HiC_Human21 persist files
-#persist.dir = "/Users/ltamon/Database/HiC_features_GSE87112_RAWpc"
-persist.dir = "/t1-home/icbml/ltamon/Database/HiC_features_GSE87112_RAWpc"
-#output.dir = paste0(objective.dir, "/out_AnnotationVsPersist_test")    
-output.dir = paste0(objective.dir, "/out_AnnotationVsPersist")    
 
-library(foreach)
-library(data.table) 
-library(IRanges) 
+if( !is.null(whorunsit[1]) ){
+  # This can be expanded as needed ...
+  if(whorunsit == "LiezelCluster"){
+    # Main directory for the task
+    objective.dir = "/t1-data/user/ltamon/ProjectBoard/_GenomicContactDynamics/3_AnnotationVsPersist"
+    # Annotation file
+    annoFile.dir   = "/t1-home/icbml/ltamon/Database/ucsc_tables"
+    # HiC_Human21 persistent contact files
+    persist.dir   = "/t1-home/icbml/ltamon/Database/HiC_features_GSE87112_RAWpc"
+    os = "Linux"
+  } else if(whorunsit == "LiezelMac"){
+    objective.dir = "/Users/ltamon/ProjectBoard/_GenomicContactDynamics/3_AnnotationVsPersist"
+    annoFile.dir   = "/Users/ltamon/Database/ucsc_tables"
+    persist.dir   = "/Users/ltamon/Database/HiC_features_GSE87112_RAWpc"
+    os = "Mac"
+  } else if(whorunsit == "AlexMac"){
+    objective.dir = "./"
+    annoFile.dir   = "/Volumes/Data/Database/ucsc_tables"
+    persist.dir   = "/Volumes/Data/Database/HiC_features_GSE87112_RAWpc"
+    os = "Mac"
+  } else {
+    print("The supplied <whorunsit> option is not created in the script.", quote=FALSE)
+  }
+}
 
-#length of ij bins
+output.dir = paste0(objective.dir, "/out_AnnotationVsPersist")
+### OTHER SETTINGS #############################################################
+# "2"(2MB gap) or "05"(0.5 MB minimum gap), refers to a minimum gap accepted to
+# further process a given contact: two points should be far enough to filter for
+# contacts within a TAD.
+gc.v = c("2")
+chr.v = c(1:22, "X")
+
+# Length of ij bins
 bin.length = 40000L
 
-#genome version
+# Genome version
 genome.ver = "hg19"
-anno.filename = paste0(genome.ver, "anno") 
+anno.filename = paste0(genome.ver, "anno")
 
-#2(2MB gap) or "05"(0.5 MB gap), refers to minimum gap between contacting bins, 
-#two points should be far enough to filter for contacts within a TAD
-gc.v = c("2")
-
-chr.v = c(1:22, "X") 
-
-#set overlap minimum and gap maximum for overlap of transcript/annotations 
-#with ij bins
-olap.min = 1L 
-gap.max = 0L #default for MAC (-1L) for Linux (0L)
-
+# Set overlap minimum and gap maximum for overlap of transcript/annotations
+# with ij bins
+olap.min = 1L
+#*** os is defined in whorunsit block
+if( os == "Linux" ){
+  gap.max = 0L
+} else if( os == "Mac" ){
+  gap.max = -1L
+}
+################################################################################
+# LIBRARIES & DEPENDANCES * LIBRARIES & DEPENDANCIES * LIBRARIES & DEPENDANCES *
+################################################################################
+library(foreach)
+library(data.table)
+library(IRanges)
 source(paste0(lib, "/TrantoRextr/GEN_WhichOverlap.R"))
+################################################################################
+# MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE *
+################################################################################
 
-################################################################################ 
-################################################################################ 
-#load annotation file 
-anno.file <- fread(file=paste0(annoFile.dir, "/", anno.filename), 
+# load annotation file
+anno.file <- fread(file=paste0(annoFile.dir, "/", anno.filename),
                    header=TRUE, data.table=FALSE)
-#check for missing values in annotation file
+# check for missing values in annotation file
 if( sum(apply(anno.file, MARGIN=c(1,2), function(x) any(is.na(x))))!=0 ){
   stop("Missing values in annotation file.")
 }
-#lengths of transcripts
+# lengths of transcripts
 txSize <- anno.file[,"txEnd"]-anno.file[,"txStart"]
-#just checking if txEnd always greater than txStart as assumed 
+#just checking if txEnd always greater than txStart as assumed
 #in the annotation file
 if( sum(txSize<0) ){
   stop("There are txStart > txEnd.")
-} 
+}
 
 for(gc in gc.v){
-  #work per chromosome, subset annotation file and load persist matrix per chromosome
+  # work per chromosome, subset annotation file and load persist matrix per chromosome
   ANNO.PERSIST.MX <- foreach(chr=chr.v, .inorder=TRUE, .combine="rbind") %do% {
     load(file=paste0(persist.dir, "/chr", chr, "_Persist_min", gc, "Mb.RData"))
-    #get the relevant bins (from persist matrix) for that chromosome
-    #bins sorted to increasing values so the ranges are also increasing
+    # get the relevant bins (from persist matrix) for that chromosome
+    # bins sorted to increasing values so the ranges are also increasing
     bins.uniq <- unique( c(unique(PERSIST.MX$hits[,"i"]),
                            unique(PERSIST.MX$hits[,"j"])) )
     bins.uniq <- sort(x=bins.uniq, decreasing=FALSE)
     bin.end   <- bins.uniq*bin.length
-    #subset annotation file per chromosome
+    # subset annotation file per chromosome
     subset.anno <- subset(x=anno.file, chrom==paste0("chr", chr))
-    #assign bins based on txStart-txEnd
-    #query   <- subset.anno; annotation (to be classified)
-    #subject <- bins.uniq (sorted, increasing); bins (classification)
-    olap.mx <- WhichOverlap(start.query=subset.anno[,"txStart"], 
-                            end.query=subset.anno[,"txEnd"], 
+    # assign bins based on txStart-txEnd
+    # query   <- subset.anno; annotation (to be classified)
+    # subject <- bins.uniq (sorted, increasing); bins (classification)
+    olap.mx <- WhichOverlap(start.query=subset.anno[,"txStart"],
+                            end.query=subset.anno[,"txEnd"],
                             space.query=rep("a", nrow(subset.anno)),
-                            start.subject=bin.end-bin.length+1, 
-                            end.subject=bin.end, 
+                            start.subject=bin.end-bin.length+1,
+                            end.subject=bin.end,
                             space.subject=rep("a",length(bin.end)),
                             maxgap=gap.max, minoverlap=olap.min)
-    #annotation assigned to bin
-    #note that one annotation can be assigned to multiple bins
-    col12 <- cbind(subset.anno[olap.mx[,"query"],], 
+
+    # annotation assigned to bin
+    # note that one annotation can be assigned to multiple bins
+    col12 <- cbind(subset.anno[olap.mx[,"query"],],
                    HiC21bin=bins.uniq[olap.mx[,"subject"]])
-    #select all contact pairs containing the annotation (either in i or j bin)
-    #make sure indices are unique 
+    # select all contact pairs containing the annotation (either in i or j bin)
+    # make sure indices are unique
     lst <- list()
     for( bin in unique(col12[, "HiC21bin"]) ){
-      #assumes that the contacts from PERSIST.MX are unique
+      # assumes that the contacts from PERSIST.MX are unique
       jbinpartnerOfBin <- PERSIST.MX$hits[PERSIST.MX$hits[,"i"]==bin,"j"]
       ibinpartnerOfBin <- PERSIST.MX$hits[PERSIST.MX$hits[,"j"]==bin,"i"]
+      # all the indices that are paired with bin
       hits.noNA.ij <- c(jbinpartnerOfBin, ibinpartnerOfBin)
       #indices of ij contacts in persist matrix that has the bin of interest
-      ind.all <- unique(c(which(PERSIST.MX$hits[,"i"]==bin), 
-                          which(PERSIST.MX$hits[,"j"]==bin)))  
-      #hits.NA      <- apply(persist.mx[ind.all,c(i, j)], MARGIN=c(1,2), 
+      ind.all <- unique(c(which(PERSIST.MX$hits[,"i"]==bin),
+                          which(PERSIST.MX$hits[,"j"]==bin)))
+      #hits.NA      <- apply(persist.mx[ind.all,c(i, j)], MARGIN=c(1,2),
       #                      function(x) ifelse(x==bin, x<-NA, x<-x))
       #hits.noNA.ij <- na.omit(c(rbind(hits.NA[,i], hits.NA[,j])))
-      strengthString <- apply(X=PERSIST.MX$hits[ind.all,3:23], MARGIN=1, 
+      strengthString <- apply(X=PERSIST.MX$hits[ind.all,3:23], MARGIN=1,
                               FUN=function(x){paste(x, collapse=";")})
-      tissuenames <- paste(colnames(PERSIST.MX$hits[ind.all,3:23]), 
+      tissuenames <- paste(colnames(PERSIST.MX$hits[ind.all,3:23]),
                            collapse="_")
-      all.val <- cbind(HiC21binPartner=hits.noNA.ij, 
+      all.val <- cbind(HiC21binPartner=hits.noNA.ij,
                        ntis=PERSIST.MX$ntis[ind.all],
                        strengthString,
                        SumStrength=PERSIST.MX$valsum[ind.all],
-                       SumStrengthOverNtis=PERSIST.MX$valsum[ind.all]/PERSIST.MX$ntis[ind.all])     
+                       SumStrengthOverNtis=PERSIST.MX$valsum[ind.all]/PERSIST.MX$ntis[ind.all])
       setnames(as.data.frame(all.val), old=3, new=tissuenames)
-      lst[[as.character(bin)]] <- c( HiC21bin=bin, 
+      lst[[as.character(bin)]] <- c( HiC21bin=bin,
                                      apply(all.val, MARGIN=2, function(x){paste(x, collapse=",")}) )
     }
-    ANNO.PERSIST.MX <- merge(col12, do.call(rbind, lst), 
+    ANNO.PERSIST.MX <- merge(col12, do.call(rbind, lst),
                              by.x="HiC21bin", all.x=TRUE) #keep rows of x
-    save(ANNO.PERSIST.MX, file=paste0(output.dir, "/", "chr", chr, "_min", gc, "Mb_", 
-                                      genome.ver, "_ALL_AnnoVsPersist_olapMin", 
+    save(ANNO.PERSIST.MX, file=paste0(output.dir, "/", "chr", chr, "_min", gc, "Mb_",
+                                      genome.ver, "_ALL_AnnoVsPersist_olapMin",
                                       olap.min, ".RData"))
-    ANNO.PERSIST.MX 
+    ANNO.PERSIST.MX
   }
-  save(ANNO.PERSIST.MX, file=paste0(output.dir, "/", "chrALL_min", gc, "Mb_", 
-                                    genome.ver, "_ALL_AnnoVsPersist_olapMin", 
+  save(ANNO.PERSIST.MX, file=paste0(output.dir, "/", "chrALL_min", gc, "Mb_",
+                                    genome.ver, "_ALL_AnnoVsPersist_olapMin",
                                     olap.min, ".RData"))
 }
 
--- 5_BaseAgeVsPersist/A1_BaseAgeVsPersist.R
+++ 5_BaseAgeVsPersist/A1_BaseAgeVsPersist.R
@@ -1,95 +1,118 @@
-#BaseAgeVsPersist
-start_time <- Sys.time()
-
+# BaseAgeVsPersist
 ################################################################################
-#Directories
+# FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS
+### DIRECTORY STRUCTURE ########################################################
+whorunsit = "LiezelCluster" # "LiezelMac", "LiezelCluster", "LiezelLinuxDesk",
+                            # "AlexMac", "AlexCluster"
 
-#functions to source
-#lib = "/Users/ltamon/ProjectBoard/lib"
+#*** See whether the content of the <lib>, specified below, can be moved to the
+#*** actual project folder for self consistency. Then, specify the location of
+#*** <lib> within the whorunsit block below. If you put the dependent function
+#*** in <lib> into the <objective.dir>/lib location, then the following line
+#*** outside the whorunsit block, after output.dir specification is enough:
+#***    lib = paste0(objective.dir, "/lib")
+
+# Functions to source
+# lib = "/Users/ltamon/ProjectBoard/lib"
 lib = "/t1-home/icbml/ltamon/ProjectBoard/lib"
-#main directory for task
-#objective.dir = "/Users/ltamon/ProjectBoard/_GenomicContactDynamics/5_BaseAgeVsPersist"
-objective.dir = "/t1-data/user/ltamon/ProjectBoard/_GenomicContactDynamics/5_BaseAgeVsPersist"
-#Base age data 
-#baseAge.dir = "/Users/ltamon/Database/BaseAgeEns76_splitData"
-baseAge.dir = "/t1-home/icbml/ltamon/Database/BaseAgeEns76_splitData"
-#HiC_Human21 persistent contact files
-#persist.dir = "/Users/ltamon/Database/HiC_features_GSE87112_RAWpc"
-persist.dir = "/t1-home/icbml/ltamon/Database/HiC_features_GSE87112_RAWpc"
-#***** repetition below
-#dir.create(paste0(objective.dir, "/out_BaseAgeVsPersist_sample"))
-#output.dir = paste0(objective.dir, "/out_BaseAgeVsPersist_sample") 
-output.dir = paste0(objective.dir, "/out_BaseAgeVsPersist") 
 
+if( !is.null(whorunsit[1]) ){
+  # This can be expanded as needed ...
+  if(whorunsit == "LiezelCluster"){
+    # Main directory for the task
+    objective.dir = "/t1-data/user/ltamon/ProjectBoard/_GenomicContactDynamics/5_BaseAgeVsPersist"
+    # Base age data
+    baseAge.dir   = "/t1-home/icbml/ltamon/Database/BaseAgeEns76_splitData"
+    # HiC_Human21 persistent contact files
+    persist.dir   = "/t1-home/icbml/ltamon/Database/HiC_features_GSE87112_RAWpc"
+  } else if(whorunsit == "LiezelMac"){
+    objective.dir = "/Users/ltamon/ProjectBoard/_GenomicContactDynamics/5_BaseAgeVsPersist"
+    baseAge.dir   = "/Users/ltamon/Database/BaseAgeEns76_splitData"
+    persist.dir   = "/Users/ltamon/Database/HiC_features_GSE87112_RAWpc"
+  } else if(whorunsit == "AlexMac"){
+    objective.dir = "./"
+    baseAge.dir   = "/Volumes/Data/Database/BaseAgeEns76_splitData"
+    persist.dir   = "/Volumes/Data/Database/HiC_features_GSE87112_RAWpc"
+  } else {
+    print("The supplied <whorunsit> option is not created in the script.", quote=FALSE)
+  }
+}
+
+output.dir = paste0(objective.dir, "/out_BaseAgeVsPersist")
+### OTHER SETTINGS #############################################################
+# "2"(2MB gap) or "05"(0.5 MB minimum gap), refers to a minimum gap accepted to
+# further process a given contact: two points should be far enough to filter for
+# contacts within a TAD.
+gc.v = c("2", "05")
+
+# chromosomes
+chr.v = c(1:22, "X")
+bin.length = 40000L
+################################################################################
+# LIBRARIES & DEPENDANCES * LIBRARIES & DEPENDANCIES * LIBRARIES & DEPENDANCES *
+################################################################################
 library(foreach)
 library(data.table) #for fread(), rbindlist()
-library(IRanges) #for WhichOverlap
-
-bin.length = 40000L
-
-#2(2MB gap) or "05"(0.5 MB minimum gap), refers to minimum gap accepted to classify a contact, 
-#two points should be far enough to filter for contacts within a TAD
-gc.v = c("2", "05") 
-
-#chromosomes
-chr.v = c(1:22, "X") 
-
-#gc="2"
-#chr=21
-
-################################################################################ 
+library(IRanges)    #required for WhichOverlap()
 ################################################################################
+# MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE *
+################################################################################
+start_time <- Sys.time()
+
 foreach(chr=chr.v, .inorder=TRUE) %do% {
-  baseAge.mx <- fread(file=paste0(baseAge.dir, "/chr", chr,"_BaseAgeEns76.txt"), 
+  baseAge.mx <- fread(file=paste0(baseAge.dir, "/chr", chr,"_BaseAgeEns76.txt"),
                       header=FALSE, data.table=FALSE, drop=c(1,2))
   colnames(baseAge.mx) <- c("base", "ancestor", "score", "colourRGB")
   binOfAge <- ceiling(baseAge.mx[,"base"]/bin.length)
   #no missing values in all base age datasets chr 1:2, X, Y and MT
   #actual base age is the second position
-  
-  #**** beware of gc() being garbage collector
+
+  #*** beware of gc() being garbage collector
   for(gc in gc.v){
     load(paste0(persist.dir, "/chr", chr, "_Persist_min", gc, "Mb.RData"))
-    
+
     #*** ~10.3 GB for chr1
-    
-    #get the relevant bins (from persist matrix) for that chromosome
-    #bins sorted to increasing values so the ranges are also increasing
+
+    #*** For clarity, always add one space after <#> in your comments. This is
+    #*** not required while commenting out lines of code, so only for comments.
+
+    # get the relevant bins (from persist matrix) for that chromosome
     bins.uniq <- unique( c(unique(PERSIST.MX$hits[,"i"]),
                            unique(PERSIST.MX$hits[,"j"])) )
-    
+
     #*** sorting does not mattter in the new way of the script, so depreciating the below line:
+    # bins sorted to increasing values so the ranges are also increasing
     #bins.uniq <- sort(bins.uniq, decreasing=FALSE)
-    #**** taking binOfAge operation out of the for loop since it does not depend in PERSIST.MX
-    
-    #identify ages that fall in a contacting bin
+    #*** taking binOfAge operation out of the for loop since it does not depend in PERSIST.MX
+
+    # identify ages that fall in a contacting bin
     #*** skipping the  creation of a PERMANENT ageOnContact object:
     #ageOnContact <- match(binOfAge, bins.uniq)
     ind.ageOnContact <- which(!is.na(  match(binOfAge, bins.uniq)  ))
-    
+
     #*** No need for a redundant matrix, as the components are already there
-    #mx.age.bin <- cbind(  bin=binOfAge[ind.ageOnContact], 
+    #mx.age.bin <- cbind(  bin=binOfAge[ind.ageOnContact],
     #                      baseAge.mx[ind.ageOnContact,]   )
-  
-    #*** restructuring the code to skip the usage of mx.age.bin  
+
+    #*** restructuring the code to skip the usage of mx.age.bin
     count.indv.base.b <- by( data = baseAge.mx[ind.ageOnContact,"base"],
-                             INDICES = binOfAge[ind.ageOnContact], 
+                             INDICES = binOfAge[ind.ageOnContact],
                              FUN = function(x) length(x) )
 
     indv.base.b   <- by( data = baseAge.mx[ind.ageOnContact,"base"],
-                         INDICES = binOfAge[ind.ageOnContact], 
+                         INDICES = binOfAge[ind.ageOnContact],
                          FUN = function(x) paste(x, collapse = ";") )
-    
+
     indv.ancstr.b <- by( data = baseAge.mx[ind.ageOnContact,"ancestor"],
-                         INDICES = binOfAge[ind.ageOnContact], 
+                         INDICES = binOfAge[ind.ageOnContact],
                          FUN = function(x) paste(x, collapse = ";") )
-    
+
     indv.score.b  <- by( data = baseAge.mx[ind.ageOnContact,"score"],
-                         INDICES = binOfAge[ind.ageOnContact], 
+                         INDICES = binOfAge[ind.ageOnContact],
                          FUN = function(x) paste(x, collapse = ";") )
-    
+
     indv.RGB.b    <- by( data = baseAge.mx[ind.ageOnContact,"colourRGB"],
-                         INDICES = binOfAge[ind.ageOnContact], 
+                         INDICES = binOfAge[ind.ageOnContact],
                          FUN = function(x) paste(x, collapse = ";") )
     bin.assigned <- as.numeric(names(count.indv.base.b))
     if( identical(bin.assigned, unique(binOfAge[ind.ageOnContact]) )==FALSE){
@@ -101,19 +124,18 @@ foreach(chr=chr.v, .inorder=TRUE) %do% {
                             ancestor=indv.ancstr.b,
                             score=indv.score.b,
                             colourRGB=indv.RGB.b)
-    #*** thats is a good idea to wrap up a part of the code with only a certain 
+    #*** thats is a good idea to wrap up a part of the code with only a certain
     # object creation, then deleting all the rest, with a non-compulsory
     # garbage collection (gc()):
     rm( list=c("count.indv.base.b",
                "indv.base.b", "indv.ancstr.b",
                "indv.score.b", "indv.RGB.b",
                "ind.ageOnContact", "bins.uniq") )
-    save(BASEAGE.BIN.MX, file=paste0(output.dir, "/min", gc, "Mb_chr", chr,  
+    save(BASEAGE.BIN.MX, file=paste0(output.dir, "/min", gc, "Mb_chr", chr,
                                      "_bin40Kb_BaseAgeVsPersist.RData"))
   }
 }
 
 end_time <- Sys.time()
-end_time-start_time    
-
+end_time-start_time
 #rm(list=ls())
