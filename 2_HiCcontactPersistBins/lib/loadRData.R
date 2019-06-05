4da6807b793977a51a110ea6e731fbdb3472396d
Liezel Tamon <lizheltamon@gmail.com>
2019-06-05 11:48:59 +0100

    update title

--- 2_HiCcontactPersistBins/lib/loadRData.R
+++ 2_HiCcontactPersistBins/lib/loadRData.R
@@ -1,8 +1,8 @@
 ################################################################################
-#load RData object, directly saving to a variable
+# Load RData object, directly saving to a variable
 
-#source: ricardo supplemented by seanM
-#https://stackoverflow.com/questions/5577221/how-can-i-load-an-object-into-a-variable-name-that-i-specify-from-an-r-data-file
+# Source: ricardo supplemented by seanM
+# https://stackoverflow.com/questions/5577221/how-can-i-load-an-object-into-a-variable-name-that-i-specify-from-an-r-data-file
 
 ###############################################################################
 loadRData <- function(fileName){
