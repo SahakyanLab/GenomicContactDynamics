1ee0afbcfc0bb2c37400a20ac71a05f135ac0636
Liezel Tamon <lizheltamon@gmail.com>
2021-08-18 15:23:30 +0800

    add code to plot DAVID results

---
+++ 16_GeneVsPersist/A7.5_FunxAnno_indiv_DAVID.R
@@ -0,0 +1,134 @@
+################################################################################
+# Plot DAVID functional annotation clusters of terms enriched in given gene set.
+################################################################################
+# FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS
+### DIRECTORY STRUCTURE ########################################################
+whorunsit = "LiezelMac" # "LiezelMac", "LiezelCluster", "LiezelLinuxDesk",
+# "AlexMac", "AlexCluster"
+
+if( !is.null(whorunsit[1]) ){
+  # This can be expanded as needed ...
+  if(whorunsit == "LiezelMac"){
+    lib = "/Users/ltamon/DPhil/lib"
+    wk.dir = "/Users/ltamon/DPhil/GCD_polished/16_GeneVsPersist"
+  } else {
+    print("The supplied <whorunsit> option is not created in the script.", quote=FALSE)
+  }
+}
+
+david.dir = paste0(wk.dir, "/out_DAVID/funxAnnoClust_sampleGeneList")
+out.dir = paste0(wk.dir, "/out_FunxAnno_indiv_DAVID")
+### OTHER SETTINGS #############################################################
+data.id = "min2Mb_ALL_cp_21_name2"
+seed.v = c(287, 587, 754)
+N = 2999
+stringency = "medium"
+
+# Get only top 10 clusters with highest enrichment score
+Nclust = 10
+################################################################################
+# LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES 
+################################################################################
+library(stringr)
+library(RColorBrewer)
+library(ggplot2)
+source(paste0(lib, "/GG_bgr.R"))
+################################################################################
+# MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE *
+################################################################################
+out.id <- paste0(data.id, "_top", Nclust, "highestEASEclusters_seed",
+                 paste(seed.v, collapse="_"), "_", N, "sampled_ftc_", stringency)
+  
+FTC <- list()
+ANME <- list() # Annotation cluster name and enrichment score
+for( seed in as.character(seed.v) ){
+  
+  FTC[[seed]] <- readLines(con=paste0(david.dir, "/", data.id, "_seed", seed, 
+                                      "_", N, "sampled_ftc_", stringency, ".txt"))
+  
+  # Remove empty strings usually before and after cluster name
+  FTC[[seed]] <- FTC[[seed]][FTC[[seed]]!=""]
+  
+  # Remove headers ("Category\tTerm\tCount") per cluster
+  headr.TF <- grepl(x=FTC[[seed]], pattern="Category\tTerm\tCount", fixed=T)
+  headr <- FTC[[seed]][headr.TF][1] # Store
+  headr <- strsplit(x=headr, split="\t", fixed=T)[[1]]
+  FTC[[seed]] <- FTC[[seed]][!headr.TF]
+  
+  # Indices of cluster names
+  cind.v <- which(grepl(x=FTC[[seed]], pattern="Annotation Cluster", fixed=T))
+  
+  # Check that number of headers same as number of clusters
+  if( sum(headr.TF)!=length(cind.v) ){
+    stop(paste0(seed, ": Checkpoint 1."))
+  } 
+  
+  # Get top 10 clusters
+  FTC[[seed]] <- FTC[[seed]][ 1:(cind.v[Nclust+1]-1) ]
+  
+  # Get cluster name, enrichment score and number of member terms
+  anme <- paste(FTC[[seed]][ cind.v[1:Nclust] ], 
+                # Number of terms per cluster
+                ( diff(cind.v[1:(Nclust+1)]) - 1 ), sep="\t")
+  anme <- gsub(x=anme, pattern="Annotation Cluster", replacement="", fixed=T)
+  anme <- gsub(x=anme, pattern="Enrichment Score:", replacement="", fixed=T)
+  
+  # Transform FTC to dataframe
+  FTC[[seed]] <- FTC[[seed]][ -cind.v[1:10] ]
+  FTC[[seed]] <- read.delim(text=FTC[[seed]], sep="\t", stringsAsFactors=F, header=F)
+  colnames(FTC[[seed]]) <- headr
+  
+  # Add cluster info to dataframe
+  anme <- read.delim(text=anme, sep="\t", stringsAsFactors=F, header=F)
+  colnames(anme) <- c("Cluster", "EASE", "Nterms")
+  if( sum(anme$Nterms)==length(FTC[[seed]][[1]]) ){
+    
+    for( nme in c("Cluster", "EASE") ){
+      
+      anme[[nme]] <- stringr::str_trim(string=anme[[nme]], side="both")
+      anme[[nme]] <- as.numeric(as.character(anme[[nme]]))
+      FTC[[seed]][[nme]] <- unlist(mapply(FUN=rep, x=anme[[nme]], each=anme$Nterms))
+      
+    }
+    
+  } else {
+    stop(paste0(seed, ": Checkpoint 2."))
+  }
+  
+  FTC[[seed]]$seed <- as.character(seed)
+  
+  rm(anme, cind.v)
+
+}
+
+FTC <- do.call("rbind", FTC)
+save(FTC, file=paste0(out.dir, "/", out.id, "_DAVIDplot.RData"))
+
+# Take only terms significant in at least one sample;
+# Use Benjamini to match the BH p-adjusted values plotted for KEGG and GO
+FTC <- FTC[ FTC$Term%in%FTC$Term[FTC$Benjamini<0.05], ]
+FTC <- FTC[order(FTC$Benjamini, decreasing=F), ]
+
+FTC$Term <- factor(x=FTC$Term, levels=rev(unique(FTC$Term)))
+FTC$Cluster <- factor(x=as.character(FTC$Cluster), 
+                      levels=as.character(sort( unique(FTC$Cluster)), decreasing=F ))
+
+p <- ggplot(data=FTC, aes(x=-log10(Benjamini), y=Term) ) +
+  geom_point(aes(colour=Cluster, size=Count)) + 
+  geom_vline(xintercept=-log10(0.05), linetype="dashed", colour="red", size=0.7) +
+  scale_colour_manual(values=brewer.pal(Nclust, "Spectral")) + 
+  guides(colour="legend") +
+  labs(title=paste0(out.id, "_reddashedlineis-log10(0.05)"), colour="Cluster",
+       size="Gene count", y=NULL, x=bquote(bold( "-log10("~"p-value"^"adj Benjamini"~")" )) 
+  ) +
+  bgr5 + 
+  facet_grid(~seed)
+
+ggsave(filename=paste0(out.dir, "/", out.id, "_DAVIDplot.pdf"), units="in",
+       width=10, height=10)
+
+# rm(list=ls()); gc()
+
+
+
+
--- 16_GeneVsPersist/A9_sampleGeneList.R
+++ 16_GeneVsPersist/A9_sampleGeneList.R
@@ -10,40 +10,65 @@ if( !is.null(whorunsit[1]) ){
   # This can be expanded as needed ...
   if(whorunsit == "LiezelMac"){
     lib = "/Users/ltamon/DPhil/lib"
-    wk.dir = "/Users/ltamon/DPhil/GenomicContactDynamics/5_GeneVsPersist"
+    wk.dir = "/Users/ltamon/DPhil/GCD_polished/16_GeneVsPersist"
   } else {
     print("The supplied <whorunsit> option is not created in the script.", quote=FALSE)
   }
 }
 genelist.dir = paste0(wk.dir, "/out_geneFilePerCp")
-unmapped.dir = paste0(wk.dir, "/out_DAVID")
+unmapped.dir = paste0(wk.dir, "/out_DAVID/unmapped")
 out.dir = paste0(wk.dir, "/out_sampleGeneList")
 ### OTHER SETTINGS #############################################################
 # gcb + refseq
 prefix = "min2Mb_ALL"
-SEED = 921
-N = 3000
+SEED = 287
+# Limit of genes that can be inputted to DAVID is 3000 but used 2999 cause 
+#sometimes DAVID maps 3000 HUGO names to 3001 DAVID IDs.
+N = 2999 
+name.type = "name2" # "entrezID" | "name2"
 ################################################################################
 # LIBRARIES & DEPENDANCES * LIBRARIES & DEPENDANCIES * LIBRARIES & DEPENDANCES *
 ################################################################################
 ################################################################################
 # MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE * MAIN CODE *
 ################################################################################
-file.v <- list.files(path=genelist.dir, pattern=prefix)
-file.v <- file.v[grep(x=file.v, pattern="entrezID")]
+file.v <- list.files(path=genelist.dir, pattern=prefix, recursive=F)
+file.v <- file.v[grep(x=file.v, pattern=name.type)]
 
 for(fle in file.v){
+  
   fle <- strsplit(x=fle, split=".", fixed=TRUE)[[1]][1]
   genes <- readLines(con=paste0(genelist.dir, "/", fle, ".txt"))
-  # Remove unmapped genes by DAVID
-  unmapped <- read.delim(file=paste0(unmapped.dir, "/", fle, "_genesNotInOut.txt"),
-                         stringsAsFactors=FALSE, header=TRUE, sep="\t")
-  unmapped <- rownames(unmapped)
-  genes <- genes[!genes%in%unmapped]
-  set.seed(SEED)
-  sampled <- sample(x=genes, size=N, replace=FALSE)
-  writeLines(sampled, con=paste0(out.dir, "/", fle, "_seed", SEED, "_sampled.txt"))
+
+  # Save original gene list
+  writeLines(unique(genes), con=paste0(out.dir, "/", fle, "_original.txt"))
+  
+  # If file of unmapped genes by DAVID is there, remove those genes.
+  unmapped.file <- paste0(unmapped.dir, "/", fle, "_DAVIDunmapped.txt")
+  if( file.exists(unmapped.file) ){
+    
+    print(paste0(fle, ": Removing unmapped then sampling."))
+    
+    unmapped <- readLines(con=unmapped.file)
+    genes <- genes[!genes%in%unmapped]
+    
+    # Sample
+    set.seed(SEED)
+    sampled <- sample(x=genes, size=N, replace=FALSE)
+    writeLines(sampled, con=paste0(out.dir, "/", fle, "_seed", SEED, "_",
+                                   N, "sampled.txt"))
+    
+    rm(sampled)
+    
+  }
+  
   print(fle, quote=FALSE)
+  
+  rm(unmapped.file, fle, genes)
+  
 }
 
-# rm(list=ls())
+# name 2; Cp=21 - out of 4221 --> 3941 DAVID IDs
+# entrezID; Cp=21 - out of 3796 --> 3796 DAVID IDs
+
+# rm(list=ls()); gc()
