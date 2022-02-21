004d1ed8e190c9ed1386f81410672574eccc2f90
Liezel Tamon <lizheltamon@gmail.com>
2022-02-21 20:49:39 +0800

    Fold change frijCp

--- 7_FeaturePermutation/D3_frijPrCp.R
+++ 7_FeaturePermutation/D3_frijPrCp.R
@@ -14,7 +14,7 @@ whorunsit = "LiezelMac" # "LiezelMac", "LiezelCluster", "LiezelLinuxDesk",
 if( !is.null(whorunsit[1]) ){
   # This can be expanded as needed ...
   if(whorunsit == "LiezelMac"){
-    home.dir = "/Users/ltamon"
+    home.dir = "/Users/ltamon/"
     os = "Mac"
   } else {
     stop("The supplied <whorunsit> option is not created in the script.", quote=F)
@@ -22,7 +22,7 @@ if( !is.null(whorunsit[1]) ){
 }
 lib      = paste0(home.dir, "/DPhil/lib")
 data.dir = paste0(home.dir, "/Database")
-wk.dir   = paste0(home.dir, "/DPhil/GCD_polished/7_FeaturePermutation")
+wk.dir   = paste0(home.dir, "/SahakyanLab/GenomicContactDynamics/7_FeaturePermutation")
 
 featvscp.dir = paste0(wk.dir, "/out_foiVsij")
 out.dir = paste0(wk.dir, "/out_frijPrCp")
@@ -31,7 +31,8 @@ gcb = "min2Mb"
 olap.type = "any_querybin" # "within_querybin" | "within_queryfoi"
 foi.num.v = 194
 # To distinguish generic names e.g. none-none, append foi.num
-foi.pair.v = c("hg19_TOP2B_TF_MCF7-hg19_TOP2B_TF_MCF7.194.")#,
+foi.pair.v = c("hg19_TOP2B_TF_MCF7-hg19_TOP2B_TF_MCF7.194.",
+               "none-hg19_TOP2B_TF_MCF7.194.") #,
 #foi.pair.v = c("none-hg19_TOP2B_TF_MCF7.194.")#,
 #               "ESC_TOP2B_TF_MCF7-ESC_TOP2B_TF_MCF7.195.",
 #               "none-ESC_TOP2B_TF_MCF7.195.",
@@ -39,9 +40,11 @@ foi.pair.v = c("hg19_TOP2B_TF_MCF7-hg19_TOP2B_TF_MCF7.194.")#,
 #               "none-FC_TOP2B_TF_MCF7.196.",
 #               "LC_TOP2B_TF_MCF7-LC_TOP2B_TF_MCF7.197.",
 #              "none-LC_TOP2B_TF_MCF7.197.")
-col.v = "#00A087FF" # Order should correspond to foi.pair.v
+combinePairs = T # If T, add counts 
+col.v = "gray40" #c("#00A087FF", "#F39B7FFF") # Order should correspond to foi.pair.v
 #col.v = "#F39B7FFF"
-out.id = "hg19_TOP2B-TOP2B_TF_MCF7"
+out.id = "hg19_TOP2B-TOP2B_TF_MCF7_hg19_none-TOP2B_TF_MCF7"
+#out.id = "hg19_TOP2B-TOP2B_TF_MCF7"
 #out.id = "hg19_none-TOP2B_TF_MCF7"
 ################################################################################
 # LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES 
@@ -69,8 +72,8 @@ for(foi.num in foi.num.v){
     allijPCp <- FEATVSCP.MX[, "All"]
     FEATVSCP.MX <- FEATVSCP.MX[, dimnames(FEATVSCP.MX)[[2]]!="All"]
     
-    # Convert counts of contacts to fraction per Cp
-    FEATVSCP.MX <- FEATVSCP.MX/allijPCp
+    ## Convert counts of contacts to fraction per Cp
+    #FEATVSCP.MX <- FEATVSCP.MX/allijPCp
     
     # Removed this checkpoint because of precision problems with float
     #if( any(rowSums(FEATVSCP.MX)!=1) ){
@@ -99,6 +102,17 @@ if( !identical(foi.pair.v, foi.pair1.v) ){
 }
 rm(foi.pair1.v)
 
+ncol.tmp <- ncol(FEATVSCP)
+if(combinePairs){
+  
+  FEATVSCP <- as.matrix(rowSums(x=FEATVSCP, na.rm=F), ncol=ncol.tmp)
+  foi.pair.v <- paste(foi.pair.v, collapse="_")
+  dimnames(FEATVSCP)[[2]] <- foi.pair.v
+  
+}
+
+FEATVSCP <- FEATVSCP/allijPCp
+  
 # Plot
 
 Cp.levels <- as.character(dimnames(FEATVSCP)[[1]])
@@ -113,7 +127,7 @@ df$variable <- as.character(df$variable)
 df$variable <- factor(x=df$variable, 
                       levels=intersect(foi.pair.v, unique(df$variable)))
 
-out.name <- paste0(gcb, "_", olap.type, "_", out.id)
+out.name <- paste0(gcb, "_", olap.type, "_", out.id, "_combinePairs", combinePairs)
 p <- ggplot(data=df, aes(x=Cp, y=value)) + 
   geom_point(aes(colour=variable), size=5) +
   #ggsci::scale_colour_npg() + 
@@ -124,6 +138,21 @@ p <- ggplot(data=df, aes(x=Cp, y=value)) +
         aspect.ratio=0.40)
 
 ggsave(filename=paste0(out.dir, "/", out.name, "_frijVsCp_scatter.pdf"),
+       width=10, height=5, plot=p)
+
+
+# Fold change
+df$value <- log2(df$value/df$value[df$Cp==1])
+p <- ggplot(data=df, aes(x=Cp, y=value)) + 
+  geom_point(aes(colour=variable), size=5) +
+  #ggsci::scale_colour_npg() + 
+  scale_colour_manual(values=col.v) + 
+  labs(y="Fold change", title=out.name) + 
+  bgr2 +
+  theme(legend.text=element_text(size=1),
+        aspect.ratio=0.40)
+
+ggsave(filename=paste0(out.dir, "/", out.name, "_foldchange_frijVsCp_scatter.pdf"),
        width=10, height=5, plot=p)
 
 # rm(list=ls()); gc()
