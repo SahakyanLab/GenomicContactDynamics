b566be93a44d649fce2f1498ab3226f5823c0b9c
Liezel Tamon <lizheltamon@gmail.com>
2023-02-13 17:48:09 +0000

    Finalise code for contact-wise plot, still for checking

--- 20_ReplicationTiming/A3_contact_Cp_plot.R
+++ 20_ReplicationTiming/A3_contact_Cp_plot.R
@@ -1,8 +1,8 @@
 ################################################################################
 # Generate contact-wise plot of RT vs. Cp. Calculate p-values i.e. ANOVA/KRUSKAL, 
 # correlation, pairwise comparison of Cp distributions plus each Cp distribution 
-# vs. all long-range contacts. Generate plots and do p-value calculations per 
-# rt.type (i.e. all, nontumor, tumor). 
+# vs. all long-range contacts (Cp=0). Generate plots and do p-value calculations 
+# per rt.type (i.e. all, nontumor, tumor). 
 ################################################################################
 # FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS * FLAGS
 ### DIRECTORY STRUCTURE ########################################################
@@ -37,6 +37,8 @@ chrs = paste0("chr", c(21:22))
 nCPU = 1
 src.id = "ij.fnxmean_allrttypes_mean_normNot1_setpointcountGrEq3_40000bpHiCres"
 Cp.v = 1:21
+
+rt.type.cols = c(all="#c1cdc1", nontumor="#3288BD", tumor="#e25d6c")
 ################################################################################
 # LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES * LIBRARIES & DEPENDENCIES 
 ################################################################################
@@ -45,11 +47,16 @@ library(doParallel)
 library(itertools)
 source(paste0(lib, "/UTL_doPar.R"))
 
+# Summary statistics
+library(Rmisc)
+
 library(reshape2)
 library(gghalves)
 library(ggplot2)
 source(paste0(lib, "/GG_bgr.R"))
+library(colorspace) # lighten colour
 
+library(car) # ANOVA for unbalanced dataset
 source(paste0(lib, "/doVarTest.R"))
 source(paste0(lib, "/doCorTest.R")) 
 source(paste0(lib, "/compareManyDist.R"))  # Update deva copy
@@ -77,29 +84,81 @@ df <- foreach(itr=isplitVector(1:chrs.len, chunks=nCPU), .combine="rbind", .inor
 }
 ### END OF PARALLEL EXECUTION ###
 
-# Transform df for plotting
+# Melt df, Convert Cp to factors
 
 df <- as.data.frame(df)
 df$Cp <- factor(as.character(df$Cp), levels=as.character(Cp.v))
 df <- reshape2::melt(df, id="Cp")
 colnames(df) <- c("Cp", "rt.type", "value")
 
-# Plot
+# df summary statistics
 
-rt.type.cols <- c(all="honeydew3", nontumor="#3288BD", tumor="#e25d6c")
+df.summ.SE <- summarySE(df, measurevar="value", groupvars=c("Cp","rt.type"), .drop=F)
+df.summ.SE.Cp0 <- cbind(Cp="0", summarySE(df, measurevar="value", groupvars="rt.type", .drop=F))
+df.summ.SE <- rbind(df.summ.SE.Cp0, df.summ.SE)
+
+SUMM.STAT <- list(summ.SE=df.summ.SE)
+save(SUMM.STAT, file=paste0(out.dir, "/", src.id, "_summary_statistics.RData"))
+rm(SUMM.STAT)
+
+# Plot mean + 95% CI
+
+df.summ.SE$Cp <- factor(as.character(df.summ.SE$Cp), levels=as.character(c("0", Cp.v)))
+df.summ.SE$rt.type <- factor(as.character(df.summ.SE$rt.type), levels=names(rt.type.cols))
+
+pd <- position_dodge(0.3)
+p <- ggplot(data=df.summ.SE, aes(x=Cp, y=value)) +
+  geom_errorbar(aes(ymin=value - ci, ymax=value + ci, col=rt.type), 
+                width=1, linewidth=0.6, position=pd) + 
+  stat_summary(aes(col=rt.type), fun="mean", size=0.35, position=pd) +
+  scale_y_continuous(limits=c(-0.75, 0.25)) + 
+  scale_colour_manual(values=rt.type.cols[levels(df.summ.SE$rt.type)]) +
+  labs(title=paste0(src.id, "_meanPlus95PercCI")) + 
+  bgr1
+
+ggsave(filename=paste0(out.dir, "/", src.id, "_meanPlus95PercCI.pdf"), width=10, height=10, 
+       plot=p)
+
+
+# Plot mean + 95% CI (but translated to set Cp=0 means to 0)
+
+# Translate Cp-0 means to 0, translating other Cp values accordingly
+df.summ.SE$value.tr <- NA
+for( rt.type in levels(df.summ.SE$rt.type) ){
+  
+  Cp0.mean.val <- df.summ.SE$value[df.summ.SE$Cp == "0" & df.summ.SE$rt.type == rt.type]
+  df.summ.SE$value.tr[df.summ.SE$rt.type == rt.type] <- df.summ.SE$value[df.summ.SE$rt.type == rt.type] - Cp0.mean.val
+  
+}
+
+p <- ggplot(data=df.summ.SE, aes(x=Cp, y=value.tr)) +
+  geom_errorbar(aes(ymin=value.tr - ci, ymax=value.tr + ci, col=rt.type), 
+                width=1, linewidth=0.6, position=pd) + 
+  stat_summary(aes(col=rt.type), fun="mean", size=0.35, position=pd) +
+  scale_y_continuous(limits=c(-0.75, 0.25)) + 
+  scale_colour_manual(values=rt.type.cols[levels(df.summ.SE$rt.type)]) +
+  labs(title=paste0(src.id, "_meanPlus95PercCI_meanTranslatedtoSetCp0MeanAt0")) + 
+  bgr1
+
+ggsave(filename=paste0(out.dir, "/", src.id, "_meanPlus95PercCI_Cp0MeanAt0.pdf"), width=10, height=10, 
+       plot=p)
+
+# Boxplots of rt values vs. Cp generated per rt.type, calculate p-values
+
 for( rt.type in unique(df$rt.type) ){
   
   df.tmp <- df[df$rt.type == rt.type, ]
   df.tmp <- na.omit(df.tmp)
   bp.stat <- boxplot.stats(x=df.tmp$value)$stats
   
+  rt.type.col.light <- rgb(hex2RGB(rt.type.cols[[rt.type]])@coords * 0.8)
   plot.title <- paste0(rt.type, "_", src.id, "_solidLineMedianALL_dashed1st3rdQuartileAll")
   out.name <- paste0(rt.type, "_", src.id)
   
   p <- ggplot(data=df.tmp, aes(x=Cp, y=value)) +
     geom_hline(yintercept=bp.stat[[3]], col="black") + 
     geom_hline(yintercept=bp.stat[c(2,4)], col="gray50", lty="dashed") + 
-    geom_half_violin(side="r", fill=rt.type.cols[[rt.type]], scale="width", lwd=0.6, 
+    geom_half_violin(side="r", fill=rt.type.col.light, scale="width", lwd=0.6, 
                      width=0.8, trim=T) +
     geom_half_boxplot(fill=rt.type.cols[[rt.type]], lwd=0.6, width=0.5, outlier.shape=1) + 
     scale_y_continuous(limits=c(-2,2)) + 
@@ -110,25 +169,51 @@ for( rt.type in unique(df$rt.type) ){
   ggsave(filename=paste0(out.dir, "/", out.name, ".png"),
          width=10*300, height=10*300, plot=p, units="px")
   
-  message(paste0(rt.type, " done!"))
-  
-  # P-values
+  rm(p)
+
+  # P-values, ANOVA/KW and correlation tests
   
   try(doVarTest(xval=df.tmp$value, grp=df.tmp$Cp, out.dir=out.dir, out.name=out.name))
   
-  try(doCorTest(xval=as.numeric(as.character(df.tmp$Cp)), yval=df.tmp$value, alt="two.sided", 
+  try(doCorTest(xval=as.numeric(as.character(df.tmp$Cp)), yval=df.tmp$value, alt="two.sided",
                 exactpval=F, out.dir=out.dir, out.name=out.name))
   
-  # All Cp=0 (all contacts) to data for ALL vs. each Cp p-value calculation
+  # Treat ALL long-range contact data distribution as extra Cp i.e. Cp = 0 then do
+  # Cp pairwise comparisons
   
+  # Add Cp=0
   df.tmp$Cp <- as.character(df.tmp$Cp)
   df.tmp.all <- df.tmp
   df.tmp.all$Cp <- "0"
-  df.tmp <- rbind(df.tmp, df.tmp.all)
+  df.tmp <- rbind(df.tmp.all, df.tmp)
+  rm(df.tmp.all)
   df.tmp$Cp <- factor(as.character(df.tmp$Cp), levels=as.character(c("0", Cp.v)))
   
-  try(compareManyDist(xval=df.tmp$value, grp=df.tmp$Cp, alt="two.sided", out.dir=out.dir, out.name=out.name))
+  try(compareManyDist( xval=df.tmp$value, grp=df.tmp$Cp, alt="two.sided", out.dir=out.dir, 
+                       out.name=paste0(out.name, "_Cp0To21compare") ))
+  
+  rm(df.tmp)
+  
+  message(paste0(rt.type, " done!"))
   
 }
+
+## Other p-values
+
+# P-values, pairwise comparisons per rt.type, per Cp 1 to 21
+
+try(compareManyDist( xval=df$value, grp=paste0(df$Cp, df$rt.type), alt="two.sided", out.dir=out.dir, 
+                     out.name=paste0(src.id, "_Cp1To21compare") ))
+
+try(compareManyDist( xval=df$value, grp=df$rt.type, alt="two.sided", out.dir=out.dir, 
+                     out.name=paste0(src.id, "_Cp0CompareRt.types") ))
+
+# P-values, two-way (Cp and rt.type) ANOVA, Kruskal-Wallis
+# Refer to http://www.sthda.com/english/wiki/two-way-anova-test-in-r#compute-two-way-anova-test-in-r-for-unbalanced-designs
+
+TEST <- list()
+TEST[["aov2"]] <- aov(value ~ Cp * rt.type, data=df)
+TEST[["aov2_unbTyp3"]] <- Anova(TEST[["aov2"]], type="III")
+save(TEST, file=paste0(out.dir, "/", src.id, "_aov2_varbasedtest.RData"))
 
 # rm(list=ls()); gc()
