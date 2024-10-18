9ccfaaa3a743b8889c02bea850534bb003bc16bf
liezeltamon <lizheltamon@gmail.com>
2024-10-18 23:58:04 +0100

    Add function to calculate significance of overlap

---
+++ lib/do_permute_test.R
@@ -0,0 +1,55 @@
+do_permute_test <- function (control_set, obs_set, olap_set, seed_val = 290, n_iter = 1000) {
+
+  #control_set <- unique(CpGenes$HiC_all)
+  #obs_set <- unique(unlist(CpGenes[as.character(1)]))
+  #olap_set <- unique(NRgenes)
+  
+  control_set <- unique(control_set)
+  obs_set <- unique(obs_set)
+  olap_set <- unique(olap_set)
+    
+  set.seed(seed_val)  # For reproducibility
+  
+  # Function to calculate percentage overlap of input with olap_set
+  calculate_overlap <- function(input, olap_set) {
+    overlap <- length(intersect(input, olap_set))
+    return(overlap / length(input) * 100)  # Percentage overlap
+  }
+  
+  observed_overlap <- calculate_overlap(obs_set, olap_set)
+  
+  # 2. Permutation test: Randomly permute gene sets and calculate the overlap for each
+  permuted_overlaps <- numeric(n_iter)
+  obs_set_size <- length(obs_set)
+  
+  for (i in 1:n_iter) {
+    
+    permuted_control <- sample(control_set, obs_set_size, replace = FALSE)
+    permuted_overlaps[i] <- calculate_overlap(permuted_control, olap_set)
+    
+  }
+  
+  # Calculate the p-value for a one-tailed test (observed overlap is lower than most permuted values)
+  cat("Permuted Overlap:\n")
+  print(summary(permuted_overlaps))
+  message("\n")
+  message("Observed Overlap:", observed_overlap, "\n")
+  
+  higher_count <- sum(permuted_overlaps >= observed_overlap)
+  #if (direction == "lower") {
+  if (observed_overlap < mean(permuted_overlaps)) {
+    p_value <- sum(permuted_overlaps <= observed_overlap) / n_iter
+    message("p-value (one-tailed, observed overlap lower):", p_value, "\n")
+  #} else if (direction == "higher") {
+  } else if (observed_overlap > mean(permuted_overlaps)) {
+    p_value <- higher_count / n_iter
+    message("p-value (one-tailed, observed overlap higher):", p_value, "\n")
+  } else if (observed_overlap == mean(permuted_overlaps)) {
+    left_tail_count <- sum(permuted_overlaps <= -abs(observed_overlap))
+    p_value <- (left_tail_count + higher_count) / n_iter
+    message("p-value (two-tailed, observed overlap):", p_value, "\n")
+  }
+  
+  return(p_value)
+   
+}
